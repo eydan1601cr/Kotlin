429706831a1022097a2bb73e7a61779ba70d152b
eydan1601cr <eydan1601@gmail.com>
2025-03-28 11:08:13 -0600

    Estructura condicional

--- src/3_Estructura_condicional_IF/problema2.kt
+++ src/3_Estructura_condicional_IF/problema2.kt
@@ -1,2 +1,26 @@
 package `3_Estructura_condicional_IF`
 
+/*
+Se ingresa por teclado un número entero comprendido entre 1 y 99, mostrar un mensaje
+ indicando si el número tiene uno o dos dígitos.
+(Tener en cuenta que condición debe cumplirse para tener
+ dos dígitos, un número entero)
+ */
+    fun main() {
+    print("Ingrese un numero del 1 al 99")
+    val v1 = readln().toInt()
+    if (v1 > 9 ) {
+        print("tiene 2 dijito")
+    }
+    else{
+        print("tiene 1 dijito")
+    }
+
+
+
+
+
+
+
+
+}
